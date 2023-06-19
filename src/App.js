3832ccb8c2d0e5200a9f3290b4d01be28b54c850
Swati Rajendra <swatirajendra@Swatis-MacBook-Pro-2.local>
2023-06-19 21:59:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import './App.css';
+
+function Counter() {
+
+  const [playerScore1,setPlayerScore1] = useState(0);
+  const [playerScore2,setPlayerScore2] = useState(0);
+  const [disabled,setDisabled] = useState(false);
+
+  return(
+    <div id='main'>
+      <div id='container'>
+        <div id='player-button-div'>
+          <button id='player1' onClick={incrementScore1} disabled={disabled}>Player-1</button>
+          <button id='player2' onClick={incrementScore2} disabled={disabled}>Player-2</button>
+        </div>
+        <div id='score-div'>
+          <p id='score1'>Player-1 Score : {playerScore1}</p>
+          <p id='score2'>Player-2 Score: {playerScore2}</p>
+        </div>
+      </div>
+    </div>
+  )
+
+  function incrementScore1() {
+    if(playerScore1 >= 10 || playerScore2 >= 10) {
+      setDisabled(true);
+    }
+    else{
+      setPlayerScore1(playerScore1+1);
+    }
+  }
+
+  function incrementScore2() {
+    if(playerScore1 >= 10 || playerScore2 >= 10) {
+      setDisabled(true);
+    }
+    else{
+      setPlayerScore2(playerScore2+1);
+    }
+  }
+}
+
+export default Counter;
